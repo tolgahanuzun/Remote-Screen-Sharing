0f51599ae9fa49ab248b3fda5abfd02e60b4aad9
tolgahanuzun <mail@tolgahanuzun.com>
2019-02-08 09:43:15 +0300

    optimization fixed

--- app.py
+++ app.py
@@ -6,10 +6,15 @@ from flask import Flask, render_template, Response, send_file, stream_with_conte
 from flask_ngrok import run_with_ngrok
 
 app = Flask(__name__)
+app.config['PRESERVE_CONTEXT_ON_EXCEPTION'] = False
+app.config['PROPAGATE_EXCEPTIONS'] = False
+app.config['DEBUG'] = False
+
 run_with_ngrok(app)
 
 def screen_grab():
     while True:
+        time.sleep(0.3)
         img_buffer = BytesIO()
         ImageGrab.grab().save(img_buffer, 'PNG', quality=1)
         img_buffer.seek(0)
@@ -20,10 +25,11 @@ def screen_grab():
 def index():
     return render_template('index.html')
 
-@app.route('/video')
-def video_feed():
+@app.route('/record')
+def record_feed():
     return Response(screen_grab(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
 if __name__ == '__main__':
     app.run()
+    time.sleep(0.3)
